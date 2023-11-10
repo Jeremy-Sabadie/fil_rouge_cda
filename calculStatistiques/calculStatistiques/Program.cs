b7ce89da892cdb549c2ab8230ff2610a6d20f6d6
cda07saba <jeremy.sabadie@2isa.org>
2023-11-10 11:35:24 +0100

    Improve and correction of the class diagram and  databases with new sql request.

---
+++ calculStatistiques/calculStatistiques/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    // Définition du délégué pour le calcul de statistiques
+    delegate double statisticalCalcul(List<double> numbers, Func<List<double>, double> calcul);
+
+    static void Main()
+    {
+        // Création d'une liste de nombres doubles
+        List<double> numbers = new List<double>();
+        Random random = new Random();
+        int i;
+        for (i = 0; i < 10; i++)
+        {
+            int nombreAleatoire = random.Next(1, 100);
+            numbers.Add(nombreAleatoire);
+
+
+        }
+        // Utilisation du délégué avec des fonctions lambda pour calculer la moyenne, la valeur minimale et maximale
+        double moyenne = CalculerStatistique(numbers, nums => nums.Average());
+        double valeurMinimale = CalculerStatistique(numbers, nums => nums.Min());
+        double valeurMaximale = CalculerStatistique(numbers, nums => nums.Max());
+
+        // Affichage des résultats
+        Console.WriteLine($"Liste de nombres : {string.Join(", ", numbers)}");
+        Console.WriteLine($"Moyenne : {moyenne}");
+        Console.WriteLine($"Valeur Minimale : {valeurMinimale}");
+        Console.WriteLine($"Valeur Maximale : {valeurMaximale}");
+    }
+
+    // Méthode générique qui utilise le délégué pour calculer une statistique
+    static double CalculerStatistique(List<double> numeros, Func<List<double>, double> calcul)
+    {
+        return calcul(numeros);
+    }
+}
+
---
+++ jalon2/bdd_queries2.sql
@@ -0,0 +1,27 @@
+--========================================
+--Creation of the material table materiel:
+CREATE  TABLE materiel(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name varchar(20),
+   fk_refCat int,
+   fk_idMat int,
+    serviceDat datetime,
+    endGarntee datetime
+--===================================================
+--Création of the material categories CategoryMateriel table:
+create table CategoryMateriel(
+    refCat INT, 
+    idMat INT 
+);
+--========================================
+--Addition of the fk_materialId foreign key on categoryMateriel table:
+
+ALTER TABLE CategoryMateriel
+ADD constraint fk_materialId FOREIGN KEY (idMat) REFERENCES materiel(id);
+--=============================================
+--Addition of the fk_categoryReference foreign key on categoryMateriel table:
+ALTER TABLE CategoryMateriel
+ADD constraint fk_categoryReference FOREIGN KEY (refCat) REFERENCES category(ref);
+--======================================================
+
+    
