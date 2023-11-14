dfd74247f9d79aeaaac6efd6364cf66a7da6598e
cda07saba <jeremy.sabadie@2isa.org>
2023-11-14 11:27:03 +0100

    Folders renamed by order.

---
+++ jalon2/04_API_AMIO_2/fil_rouge_api/Materiel.cs
@@ -0,0 +1,11 @@
+﻿namespace fil_rouge_api
+{
+    public class Materiel
+    {
+        private string name { get; set; }
+        private int fk_refCat { get; set; }
+        private int fk_idMat { get; set; }
+        private DateTime serviceDat { get; set; }
+        private DateTime endGarantee { get; set; }
+    }
+}
