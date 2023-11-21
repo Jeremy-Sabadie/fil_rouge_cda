92fa347e1bad0cc79ea9b72abf3a673759a715ca
cda07saba <jeremy.sabadie@2isa.org>
2023-11-21 13:55:31 +0100

    Materiel controller created and crud started for exemple.

---
+++ 02_jalon2/03_API_AMIO_2/apiMateriels/Controllers/MaterielController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiMateriels.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterielController : ControllerBase
+    {
+        //POST->Création:
+        [HttpPost()]
+        public async Task<IActionResult> CreateBook([FromBody] Book book)
+        {
+            //verifier
+            if (book.Title != null && book.Autor != null)
+            {
+                //faire
+                Book newbook = new(5, book.Title, book.Autor);
+                Book.books.Add(newbook);
+                //repondre
+                return Ok(newbook);
+            }
+            else
+            {
+                return BadRequest();
+            }
+            //PUT->Mise à jour:
+            [HttpPut("{id}")]
+            public IActionResult UpdateBook([FromRoute] int id, [FromBody] Book book)
+            {
+                if (Book.books.Exists(b => b.Id == id))
+                {
+                    Book b = Book.books.Find(b => b.Id == id);
+                    b.Title = book.Title;
+                    b.Autor = book.Autor;
+                    return Ok(book);
+                }
+
+                else { return NotFound(); }
+            }
+            [HttpDelete("{id}")]
+            [ProducesResponseType(StatusCodes.Status204NoContent)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
+            public async Task<IActionResult> DeleteBook([FromRoute] int id)
+            {
+                Book b = Book.books.Find(b => b.Id == id);
+                if (b is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    Book.books.Remove(b);
+                    return NoContent();
+                }
+                // GET: api/<MaterielController>
+                [HttpGet]
+                public IEnumerable<string> Get()
+                {
+                    return new string[] { "value1", "value2" };
+                }
+
+                // GET api/<MaterielController>/5
+                [HttpGet("{id}")]
+                public string Get(int id)
+                {
+                    return "value";
+                }
+
+                // POST api/<MaterielController>
+                [HttpPost]
+                public void Post([FromBody] string value)
+                {
+                }
+
+                // PUT api/<MaterielController>/5
+                [HttpPut("{id}")]
+                public void Put(int id, [FromBody] string value)
+                {
+                }
+
+                // DELETE api/<MaterielController>/5
+                [HttpDelete("{id}")]
+                public void Delete(int id)
+                {
+                }
+            }
+        }
--- 02_jalon2/03_API_AMIO_2/apiMateriels/obj/Debug/net8.0/apiMateriels.MvcApplicationPartsAssemblyInfo.cs
+++ 02_jalon2/03_API_AMIO_2/apiMateriels/obj/Debug/net8.0/apiMateriels.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("domain")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d242d409a8647b72a2abc61b827d62723ac70cf6")]
-[assembly: System.Reflection.AssemblyProductAttribute("domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("domain")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
